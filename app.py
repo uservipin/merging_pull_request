313a86d0ee5b7a038ed4531e23816c8f138afc44
Vipin <kumar.vipin1v00@gmail.com>
2024-08-12 01:23:08 +0530

    test

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 print("Hello world")
 print("Test")
 print("Hey")
+
 print("Push this code into feature branch")
 print("check direct push in main after applying rule")
 print("This is not woking")
 print("Check again test main")
+
+print("Push this code into feature branch")
+print("check direct push in main after applying rule")
+print("This is not woking")
+print("Check again test main")
+
+# added comment
+# added new comment
+# test again direct push 
